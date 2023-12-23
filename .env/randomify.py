7a0cd295404f006bcdef15b3a51d33dfa95dabde
Aatrick <113598245+Aatrick@users.noreply.github.com>
2023-12-23 22:45:23 +0100

    virtual environement + working version of randomify

--- .env/randomify.py
+++ .env/randomify.py
@@ -1,71 +1,71 @@
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import json
 import webbrowser
 import pandas as pd
 import random 
 import time
+from pynput.keyboard import Key, Controller
 
-from spotipy.oauth2 import SpotifyClientCredentials
+keyboard = Controller()
 
 username = 'Emilio'
 clientID = 'f956c798c044418b9204fe6fc9d303ed'
 clientSecret = '5ca12d350e574940852ab5dde947abdd'
 redirectURI = 'http://google.com/' 
 
-oauth_object = spotipy.SpotifyOAuth(clientID,clientSecret,redirectURI)
-token_dict = oauth_object.get_access_token()
+oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirectURI)
+#try:
+#    token_dict = oauth_object.get_cached_token()
+#except spotipy.oauth2.MaxRetries:
+#    print("Max Retries reached")
+#    # Handle the MaxRetries exception here
+#except spotipy.oauth2.ResponseError as e:
+#    if e.status == 429:
+#        print("Too many 429 error responses")
+#        # Handle the ResponseError exception here
+#    else:
+#        raise e
+token_dict = oauth_object.get_access_token() 
 token = token_dict['access_token']
 spotifyObject = spotipy.Spotify(auth=token)
 user = spotifyObject.current_user()
 
-
 client_credentials_manager = SpotifyClientCredentials(client_id=clientID, client_secret=clientSecret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
+def get_tracks(playlist_id):
+    playlist = sp.playlist_tracks(playlist_id)["items"]
 
-def call_playlist(creator, playlist_id):
-    playlist_features_list = ["artist","album","track_name",  "track_id","danceability","energy","key","loudness","mode", "speechiness","instrumentalness","liveness","valence","tempo", "duration_ms","time_signature"]
-    playlist_df = pd.DataFrame(columns = playlist_features_list)
-    
-    # Initialize offset
+    track_list = []
     offset = 0
-
     while True:
-        playlist = sp.user_playlist_tracks(creator, playlist_id, offset=offset)["items"]
+        playlist = sp.playlist_tracks(playlist_id, offset=offset)["items"]
         if not playlist:
             break
-
-        for track in playlist:
-            playlist_features = {}
-            playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
-            playlist_features["album"] = track["track"]["album"]["name"]
-            playlist_features["track_name"] = track["track"]["name"]
-            playlist_features["track_id"] = track["track"]["id"]
-            playlist_features["track_ms"] = track["track"]["duration_ms"]
-            
-            audio_features = sp.audio_features(playlist_features["track_id"])[0]
-            for feature in playlist_features_list[4:]:
-                playlist_features[feature] = audio_features[feature]
-            
-            track_df = pd.DataFrame(playlist_features, index = [0])
-            playlist_df = pd.concat([playlist_df, track_df], ignore_index = True)
-        
-        # Increase the offset by 100 (Spotify's limit per request)
         offset += 100
+        for track in playlist:
+            track_id = track["track"]["id"]
+            track_name = track["track"]["name"]
+            track_artist = track["track"]["artists"][0]["name"]
+            track_duration = int(track["track"]["duration_ms"] / 1000)
+            track_tuple = (track_id, track_name, track_artist, track_duration)
+            track_list.append(track_tuple)
+    
+    return track_list
 
-    return playlist_df[['track_name', 'track_ms']]
-
-playlist_songs = call_playlist("Emilio","19807JSM82MpH1Krr2pO7I")
+playlist_songs=get_tracks("19807JSM82MpH1Krr2pO7I")
 
 while True:
     track = random.choice(playlist_songs)
-    track_name = track['track_name']
-    track_duration = track['track_ms'] / 1000
-    searchQuery = track_name
-    searchResults = spotifyObject.search(searchQuery,1,0,"track")
-    tracks_dict = searchResults['tracks']
-    tracks_items = tracks_dict['items']
-    song = tracks_items[0]['external_urls']['spotify']
+    track_id = track[0]
+    track_name = track[1]
+    track_artist = track[2]
+    track_duration = track[3]
+    song = f"https://open.spotify.com/track/{track_id}"
     webbrowser.open(song)
-    print(f"Now playing: {track_name}")
+    time.sleep(2)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    print(f"Now playing: {track_name} \nfrom {track_artist}\n")
     time.sleep(track_duration)
